ad800afe3f6319e3c4a6b17452c2efdf41afe844
Teng Yun <davenkin@126.com>
2024-12-19 20:37:41 +0800

    clean project

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-npx tsc
+rm -rf ./dist
+npm run build
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-
-import { greet } from './utils/utils';  // Importing the greet function from greet.ts
-
-const message = greet('World');
-console.log(message);
+console.log("Hello World!");
